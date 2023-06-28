1911b61a9279246032c28d93955f2d9b8689ca20
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-06-28 17:31:47 +0200

    prettier

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -18,33 +18,39 @@ const MovieDetails = () => {
     getMovieDetails(movieId);
   }, [movieId]);
 
-const { poster_path, title, genres, vote_average, overview } = details;
+  const { poster_path, title, genres, vote_average, overview } = details;
 
   return (
     <>
       <div className={css.wrapper}>
-        <Link className={css.goBackBtn} to={backLinkRef.current}>Go back</Link>
+        <Link className={css.goBackBtn} to={backLinkRef.current}>
+          Go back
+        </Link>
         <div className={css.movieDetails}>
-        <img 
-      className={css.img}
-      src={
-        poster_path
-          ? `https://www.themoviedb.org/t/p/w500/${poster_path}`
-          : 'https://www.freeiconspng.com/uploads/no-image-icon-6.png'
-      }
-      alt={title} width='240' />
-        <div className={css.descriptionWrapper}>
-          <h3 className={css.h3}>{title}</h3>
-          <h5 className={css.h5}>User Score: {vote_average}</h5>
-          <div className={css.description}>
-            <h5 className={css.descTitle}>Overview</h5>
-            <p className={css.desc}>{overview}</p>
+          <img
+            className={css.img}
+            src={
+              poster_path
+                ? `https://www.themoviedb.org/t/p/w500/${poster_path}`
+                : 'https://www.freeiconspng.com/uploads/no-image-icon-6.png'
+            }
+            alt={title}
+            width="240"
+          />
+          <div className={css.descriptionWrapper}>
+            <h3 className={css.h3}>{title}</h3>
+            <h5 className={css.h5}>User Score: {vote_average}</h5>
+            <div className={css.description}>
+              <h5 className={css.descTitle}>Overview</h5>
+              <p className={css.desc}>{overview}</p>
+            </div>
+            <div className={css.description}>
+              <h5 className={css.descTitle}>Genres</h5>
+              <p className={css.desc}>
+                {genres && genres.map(genre => genre.name).join(', ')}
+              </p>
+            </div>
           </div>
-          <div className={css.description}>
-            <h5 className={css.descTitle}>Genres</h5>
-            <p className={css.desc}>{genres && genres.map(genre => genre.name).join(', ')}</p>
-          </div>
-        </div>
         </div>
       </div>
       <div className={css.wrapper}>
